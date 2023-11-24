b7ad722a7f4b7478373a4e44d5396fdbd02db4df
kristofvermelen <61462734+kristofvermelen@users.noreply.github.com>
2023-11-24 14:57:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-echo "Hello Azure2!"
+echo "Proximus XFF test for Umbrella"
+
+// Get the X-Forwarded-For header from the request
+$xffHeader = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : 'Not provided';
+
+// Send a response to the client with the X-Forwarded-For header value
+echo "X-Forwarded-For Header: $xffHeader";
 
 ?>
