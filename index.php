b6a6f1fafbd8653955f610e24bca64045f254590
kristofvermelen <61462734+kristofvermelen@users.noreply.github.com>
2023-11-24 15:30:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,51 @@
 <?php
-// Get the X-Forwarded-For header from the request
-$xffHeader = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : 'Not provided';
+// Function to get geolocation data for an IP address using ipinfo.io API
+function getGeolocationData($ip) {
+    // Initialize cURL session
+    $ch = curl_init();
+
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_URL, "https://ipinfo.io/$ip/json");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute cURL session and decode JSON response
+    $response = curl_exec($ch);
+    $geolocationData = json_decode($response, true);
+
+    // Close cURL session
+    curl_close($ch);
+
+    return $geolocationData;
+}
 
 // Get the actual client IP address
 $clientIp = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'Not provided';
 
-// Initialize cURL session
-$ch = curl_init();
+// Get geolocation data for the actual client IP
+$clientGeolocation = getGeolocationData($clientIp);
 
-// Set cURL options
-curl_setopt($ch, CURLOPT_URL, "https://ipinfo.io/$clientIp/json");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Get the X-Forwarded-For header from the request
+$xffHeader = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : 'Not provided';
 
-// Execute cURL session and decode JSON response
-$response = curl_exec($ch);
-$geolocationData = json_decode($response, true);
-
-// Close cURL session
-curl_close($ch);
+// Get geolocation data for the X-Forwarded-For header
+$xffGeolocation = getGeolocationData($xffHeader);
 
 // Output both the X-Forwarded-For header, actual client IP, and geolocation information
 echo "X-Forwarded-For Header: $xffHeader<br>";
 echo "Actual Client IP: $clientIp<br>";
 
-if ($geolocationData && isset($geolocationData['country'])) {
-    echo "Country: {$geolocationData['country']}<br>";
+// Output geolocation information for the actual client IP
+if ($clientGeolocation && isset($clientGeolocation['country'])) {
+    echo "Geolocation for Actual IP - Country: {$clientGeolocation['country']}<br>";
     // You can include more information like city, region, etc. depending on the API response
 } else {
-    echo "Geolocation information not available.<br>";
+    echo "Geolocation information for Actual IP not available.<br>";
 }
-?>
+
+// Output geolocation information for the X-Forwarded-For header
+if ($xffGeolocation && isset($xffGeolocation['country'])) {
+    echo "Geolocation for X-Forwarded-For - Country: {$xffGeolocation['country']}<br>";
+    // You can include more information like city, region, etc. depending on the API response
+} else {
+    echo "Geolocation information for X-Forwarded-For not available.<br>";
+}
