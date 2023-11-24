31407e7b0016e66f0ea6a2a47f9d605e3152a722
kristofvermelen <61462734+kristofvermelen@users.noreply.github.com>
2023-11-24 15:18:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php phpinfo(); ?>
+<?php
+// Get the X-Forwarded-For header from the request
+$xffHeader = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : 'Not provided';
+
+// Output the X-Forwarded-For header value
+echo "X-Forwarded-For Header: $xffHeader";
+?>
