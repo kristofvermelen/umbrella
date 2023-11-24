d6ce586a18d3125f0590efd2fcdb7b6568b23048
kristofvermelen <61462734+kristofvermelen@users.noreply.github.com>
2023-11-24 15:29:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,28 @@ $xffHeader = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDE
 // Get the actual client IP address
 $clientIp = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'Not provided';
 
-// Output both the X-Forwarded-For header and the actual client IP address
+// Initialize cURL session
+$ch = curl_init();
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_URL, "https://ipinfo.io/$clientIp/json");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute cURL session and decode JSON response
+$response = curl_exec($ch);
+$geolocationData = json_decode($response, true);
+
+// Close cURL session
+curl_close($ch);
+
+// Output both the X-Forwarded-For header, actual client IP, and geolocation information
 echo "X-Forwarded-For Header: $xffHeader<br>";
-echo "Actual Client IP: $clientIp";
+echo "Actual Client IP: $clientIp<br>";
+
+if ($geolocationData && isset($geolocationData['country'])) {
+    echo "Country: {$geolocationData['country']}<br>";
+    // You can include more information like city, region, etc. depending on the API response
+} else {
+    echo "Geolocation information not available.<br>";
+}
 ?>
