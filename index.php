2766b2c8826500f4f3eaa9f6968cc09084f18130
kristofvermelen <61462734+kristofvermelen@users.noreply.github.com>
2023-11-24 15:01:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,4 @@
 
 echo "Proximus XFF test for Umbrella"
 
-if (isset($_SERVER['HTTP_CLIENT_IP']) && array_key_exists('HTTP_CLIENT_IP', $_SERVER)) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
-    $ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-    $ips = array_map('trim', $ips);
-    $ip = $ips[0];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'] ?? '0.0.0.0';
-}
-
-$ip = filter_var($ip, FILTER_VALIDATE_IP);
-$ip = ($ip === false) ? '0.0.0.0' : $ip;
-
-echo $ip
-
 ?>
